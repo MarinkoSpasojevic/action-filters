d21b3c9c4886e0cde7de37721349398879b7d394
Marinko <marinkospasojevic@gmail.com>
2018-06-27 10:29:23 +0200

    Starting project

---
+++ ActionFilters-Start/ActionFilters-Start/Controllers/MovieController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ActionFilters_Start.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionFilters_Start.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class MovieController : Controller
+    {
+        private readonly MovieContext _context;
+
+        public MovieController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Movie
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var a = _context.Movies.ToList();
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Movie/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Movie
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Movie/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
