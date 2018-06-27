cf1f0074330ac1d0723654ebb932c42e70bc0a8b
Marinko <marinkospasojevic@gmail.com>
2018-06-27 12:49:39 +0200

    end project started

---
+++ ActionFilters-End/ActionFilters-Start/Extensions/MovieExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ActionFilters_End.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionFilters_End.Extensions
+{
+    public static class MovieExtensions
+    {
+        public static void Map (this Movie dbMovie, Movie movie)
+        {
+            dbMovie.Name = movie.Name;
+            dbMovie.Genre = movie.Genre;
+            dbMovie.Director = movie.Director;
+        }
+    }
+}
