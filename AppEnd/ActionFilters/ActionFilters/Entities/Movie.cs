f1f2dd23e82c16bfe280b7c74acd2bf7fe176090
Marinko <marinkospasojevic@gmail.com>
2018-06-27 22:30:18 +0200

    Added projects

---
+++ AppEnd/ActionFilters/ActionFilters/Entities/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ActionFilters.Entities
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+    }
+}
